b9746cdb7bfc4fae9500b830fd272342e78101a3
Connor Doman <connor@connordoman.dev>
2025-08-31 19:17:14 -0700

    add direct print command

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,11 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"log"
+
+	"github.com/connordoman/pos/internal/escpos"
+	"github.com/spf13/cobra"
+)
 
 var RootCommand = &cobra.Command{
 	Use:   "pos",
@@ -15,5 +20,22 @@ func init() {
 }
 
 func runRootCommand(cmd *cobra.Command, args []string) error {
+	p, err := escpos.InitPrinter()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := p.Close(); err != nil {
+			log.Printf("close error: %v", err)
+		}
+	}()
+
+	p.Init()
+	p.SimpleLine()
+	p.Feed(3)
+	p.Log("Hello, world!")
+	p.Feed(3)
+	p.SimpleLine()
+	p.Flush()
 	return nil
 }
