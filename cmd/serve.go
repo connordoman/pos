36ac017f0e99a6ca2aaa74c3a774699e913e8866
Connor Doman <connor@connordoman.dev>
2025-09-01 10:18:30 -0700

    fix: omg dumb

--- cmd/serve.go
+++ cmd/serve.go
@@ -173,6 +173,7 @@ func runServeCommand(cmd *cobra.Command, args []string) error {
 		markdown, err := md.Parse(text)
 		if err != nil {
 			log.Printf("markdown parse error: %v", err)
+			log.Printf("original message: %s", text)
 			http.Error(w, "Failed to parse markdown", http.StatusBadRequest)
 			return
 		}
@@ -181,7 +182,6 @@ func runServeCommand(cmd *cobra.Command, args []string) error {
 
 		if err := p.FeedAndCut(5); err != nil {
 			log.Printf("printer feed and cut error: %v", err)
-			log.Printf("original message: %s", text)
 			http.Error(w, "Failed to feed and cut paper", http.StatusInternalServerError)
 			return
 		}
